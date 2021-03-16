6d85eb4f0ca87e12de1dc596ae1dfde1cc93b44e
David Paquette <dave.paquette@outlook.com>
2021-03-16 12:26:58 -0600

    Initial commit

---
+++ OutputFormattersSample/CsvOutputFormatter.cs
@@ -0,0 +1,41 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.Collections;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputFormattersSample
+{
+    public class CsvOutputFormatter : TextOutputFormatter
+    {
+        public CsvOutputFormatter()
+        {
+            SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("text/csv"));
+            SupportedEncodings.Add(Encoding.UTF8);
+            SupportedEncodings.Add(Encoding.Unicode);
+        }
+
+        protected override bool CanWriteType(Type type)
+        {
+            if (typeof(IEnumerable).IsAssignableFrom(type))
+            {
+                return base.CanWriteType(type);
+            }
+            return false;
+        }
+
+        public override async Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
+        {
+            var streamWriter = new StreamWriter(context.HttpContext.Response.Body, selectedEncoding, leaveOpen: true);
+
+            await using (var csvWriter = new CsvWriter(streamWriter, new CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture)))
+            {
+                await csvWriter.WriteRecordsAsync((IEnumerable)context.Object);
+            }
+        }
+    }
+}
